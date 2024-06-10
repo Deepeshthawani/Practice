55fa57a7c575c96ac0185c93defb486930b06d0b
Deepeshthawani <thawanideepesh.dt@gmail.com>
2024-06-11 02:31:15 +0530

    Wire Practice

---
+++ force-app/main/default/lwc/wirePractice/wirePractice.js
@@ -0,0 +1,13 @@
+import { LightningElement, wire } from 'lwc';
+import { getListUi } from 'lightning/uiListApi';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+
+export default class WirePractice extends LightningElement {
+    @wire(getListUi , {objectApiName: ACCOUNT_OBJECT, listViewApiName: 'AllAccounts' })
+    accounts
+    
+    get accountList() {
+        return this.accounts.data ? this.accounts.data.records.records : [];
+    }
+
+}
